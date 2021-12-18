c467ebe899458e2a79fb4b087e4c8ea3d807892b
Van Tran <tavan.it@gmail.com>
2021-12-18 16:50:32 +0100

    JS Crash Dom Test

--- dom2.js
+++ dom2.js
@@ -1,3 +1,101 @@
+// Click buttons to hide or show a text
+document.getElementById('hider').onclick = function(){
+    document.getElementById('text').hidden=true;
+}
+document.getElementById('shower').onclick = function(){
+    document.getElementById('text').hidden = false;
+}
+
+
+// class Menu {
+//     handleEvent(event) {
+//       switch(event.type) {
+//         case 'mousedown':
+//           elem3.innerHTML = "Mouse button pressed";
+//           break;
+//         case 'mouseup':
+//           elem3.innerHTML += "...and released.";
+//           break;
+//       }
+//     }
+// }
+//   let menu = new Menu();
+//   elem3.addEventListener('mousedown', menu);
+//   elem3.addEventListener('mouseup', menu);
+
+
+// //Count Rabbits
+// function countRabbits(){
+//     for (i = 1; i <= 3; i++){
+//         alert("Rabbits number " + i);
+//     }
+// }
+
+// testAA.onclick = function(){
+//     alert('Thank you');
+// }
+
+// //addEvenListener
+// function handler1() {
+//     alert('Thanks!');
+//   };
+// function handler2() {
+//     alert('Thanks again!');
+//   }
+// elem.onclick = () => alert("Hello");
+// elem.addEventListener("click", handler1);
+// elem.addEventListener("click", handler2);
+
+
+// const body = document.querySelector('body');
+// const newPara = document.createElement('p');
+// newPara.innerText = 'This paragraph is inserted by DOM';
+// newPara.style.background = 'red';
+// body.appendChild(newPara);
+
+// var newDiv = document.createElement('div');
+// //Add class
+// newDiv.className = 'Hello';
+// //Add id
+// newDiv.id = 'World';
+// //Add attr
+// newDiv.setAttribute('title', 'I am Van');
+
+// // add text node
+// var newDivText = document.createTextNode('Good morning');
+// // add text to div
+// newDiv.appendChild(newDivText);
+// // console.log(newDiv);
+// var container1 = document.querySelector('header .container');
+// var new_h1 = document.querySelector('header h1');
+// //insert div 
+// container1.insertBefore(newDiv, new_h1).style.color = 'orange';
+// newDiv.style.fontSize = '40px';
+
+
+//ParentNodes:
+// var itemList = document.querySelector('#items');
+// console.log(itemList.parentNode);
+// itemList.parentNode.style.background = 'lightblue';
+
+// console.log(itemList.parentNode.parentNode);
+
+// var itemList = document.querySelector('#items');
+// console.log(itemList.parentElement);
+// itemList.parentElement.style.background = 'lightblue';
+// var itemList = document.querySelector('#items');
+// console.log(itemList.firstChild);
+// console.log(itemList.firstElementChild);
+// itemList.children[2].textContent = 'Hello2';
+// itemList.children[1].style.color = 'blue';
+// itemList.children[1].style.color = 'blue';
+// itemList.firstElementChild.textContent = 'Hello1';
+
+// console.log(itemList.nextElementSibling);
+// console.log(itemList.previousElementSibling);
+// itemList.previousElementSibling.style.color = 'red';
+
+
 // console.log(document.URL);
 // // console.log(document.all[9]);
 // // change header content
@@ -37,10 +135,10 @@
 // var titles = document.querySelectorAll('.title')
 // console.log(titles);
 
-var odd = document.querySelectorAll('li:nth-child(odd)')
-var even = document.querySelectorAll('li:nth-child(even)')
-console.log(odd);
-for(var i = 0; i < odd.length; i++){
-    odd[i].style.backgroundColor = 'gray';
-    even[i].style.backgroundColor = 'lightyellow';
-}
+// var odd = document.querySelectorAll('li:nth-child(odd)')
+// var even = document.querySelectorAll('li:nth-child(even)')
+// console.log(odd);
+// for(var i = 0; i < odd.length; i++){
+//     odd[i].style.backgroundColor = 'gray';
+//     even[i].style.backgroundColor = 'yellow';
+// }
